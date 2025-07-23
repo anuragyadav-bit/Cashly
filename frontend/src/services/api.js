c24188c9b117fad7fe6745f93b78816a821e25f7
anuragyadav-bit <anurag051104@gmail.com>
2025-07-23 15:01:16 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://cashly-e297.onrender.com';
+const API_URL = 'https://cashly-e297.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
